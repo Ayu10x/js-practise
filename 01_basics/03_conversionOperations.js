7240314ec076d8dbd217f2609794b5afd47fcc26
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2024-10-13 06:08:03 +0000

    Why string to number conversion is confusing?

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,5 +27,34 @@ let someNumber = 69
 
 let numberInString = String(someNumber)
 
-console.log(numberInString)
-console.log(typeof numberInString);
+// console.log(numberInString)
+// console.log(typeof numberInString);
+
+
+let value = 3;
+
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+ 2+ "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log([num1, num2, num3]);
+
+
+// prefix and postfix => mdn
