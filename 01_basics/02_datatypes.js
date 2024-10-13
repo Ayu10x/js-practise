7555fab5dddb55d8b99dc81911f6200de66bfcda
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2024-10-13 02:59:35 +0000

    Datatypes and ECMA standards

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version.
+
+// alert(3 + 3) // won't work here because we are using node.js not browser.
+
+let name = "Ayush"
+let age = 18
+let loggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => used for unique things
+
+
+// object
+
+console.log(typeof "Ayush")
+
+console.log(typeof null); // is a object
+
+console.log(typeof undefined); // undefined
+
