14b71748b96ffa8f3c9d05c29fc9cf6556d4d5f1
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2024-10-16 14:36:45 +0000

    docs

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+// arrays
+
+const myArr = [0, 1, 2, 3]
+// console.log(myArr);
+
+
+const myArr2 = new Array(0, 1, 2, 3, 4)
+
+// console.log(myArr2);
+
+// console.log(typeof myArr);
+// console.log(typeof myArr2);
+
+myArr2.push(6)
+// console.log(myArr2);
+
+// unshift => adds one element to the left side of the array / start of the index.
+
+// shifts the first element out of the array
+
+
+
